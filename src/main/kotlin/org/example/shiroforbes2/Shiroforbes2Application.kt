a35c7bfaed6af01ab958fc9fd555c9808464d57c
Nosorozhek <nosorozhek239@gmail.com>
2025-06-23 14:52:09 +0300

    init: initial commit

---
+++ src/main/kotlin/org/example/shiroforbes2/Shiroforbes2Application.kt
@@ -0,0 +1,11 @@
+package org.example.shiroforbes2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Shiroforbes2Application
+
+fun main(args: Array<String>) {
+    runApplication<Shiroforbes2Application>(*args)
+}
---
+++ src/test/kotlin/org/example/shiroforbes2/Shiroforbes2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.shiroforbes2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Shiroforbes2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

99f5c9e69397d6be9e834838178265a92c1ea08c
Nosorozhek <nosorozhek239@gmail.com>
2025-08-06 01:38:05 +0300

    feat: implemented some transaction routes

--- src/main/kotlin/ru/shiroforbes2/api/TransactionsController.kt
+++ src/main/kotlin/ru/shiroforbes2/api/TransactionsController.kt
@@ -2,24 +2,21 @@ package ru.shiroforbes2.api
 
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.shiroforbes2.dto.TransactionDTO
-import ru.shiroforbes2.dto.response.MessageResponse
+import ru.shiroforbes2.service.TransactionService
 
 @RestController
 @RequestMapping("/api/v2/transaction")
-class TransactionsController {
-  @GetMapping("/all")
-  @PreAuthorize("hasAuthority('Teacher')")
-  fun getAllTransactions(): List<TransactionDTO> {
-    TODO()
-  }
+class TransactionsController(
+  val transactionsService: TransactionService,
+) {
+  @GetMapping("/countryside")
+  @PreAuthorize("hasAuthority('Admin') or hasAuthority('Teacher')")
+  fun getCountrysideTransactions(): List<TransactionDTO> = transactionsService.getCountrysideTransactions()
 
-  @PostMapping("/new")
-  @PreAuthorize("hasAuthority('Teacher')")
-  fun createNewTransaction(): MessageResponse {
-    TODO()
-  }
+  @GetMapping("/urban")
+  @PreAuthorize("hasAuthority('Admin') or hasAuthority('Teacher')")
+  fun getUrbanTransactions(): List<TransactionDTO> = transactionsService.getUrbanTransactions()
 }
--- src/main/kotlin/ru/shiroforbes2/repository/TransactionRepository.kt
+++ src/main/kotlin/ru/shiroforbes2/repository/TransactionRepository.kt
@@ -1,6 +1,7 @@
 package ru.shiroforbes2.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import ru.shiroforbes2.entity.Transaction
@@ -9,4 +10,24 @@ import ru.shiroforbes2.entity.Transaction
 interface TransactionRepository : JpaRepository<Transaction, Long> {
   @Transactional(readOnly = true)
   fun findAllByStudentIdOrderByDate(studentId: Long): List<Transaction>
+
+  @Transactional(readOnly = true)
+  @Query(
+    """
+      select t from Transaction t join Student s on t.studentId = s.id
+      where s.group = 'Countryside'
+      order by t.date
+     """,
+  )
+  fun findAllCountrysideOrderByDate(): List<Transaction>
+
+  @Transactional(readOnly = true)
+  @Query(
+    """
+      select t from Transaction t join Student s on t.studentId = s.id
+      where s.group = 'Urban'
+      order by t.date
+     """,
+  )
+  fun findAllUrbanOrderByDate(): List<Transaction>
 }

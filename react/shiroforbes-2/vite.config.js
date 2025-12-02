47be5f3914a91a73638e29191c9e740ef988b0f6
Sanya239 <alex.bogdanov05@mail.ru>
2025-12-02 12:13:14 +0300

    configuration changes

--- react/shiroforbes-2/vite.config.js
+++ react/shiroforbes-2/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     },
     server: {
         host: true,
-        port: 80,
+        port: 8239,
         proxy: {
             '/api': {
                 target: 'http://localhost:8080',

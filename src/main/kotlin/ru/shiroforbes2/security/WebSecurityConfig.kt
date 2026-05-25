69768dc1e37902ac30cd2b78c36eea061a19e2c7
Sanya239 <alex.bogdanov05@mail.ru>
2026-05-25 13:58:56 +0300

    fix formatting

--- src/main/kotlin/ru/shiroforbes2/security/WebSecurityConfig.kt
+++ src/main/kotlin/ru/shiroforbes2/security/WebSecurityConfig.kt
@@ -58,7 +58,8 @@ class WebSecurityConfig(
           .permitAll()
           .requestMatchers("/api/v2/test/**")
           .permitAll()
-          .requestMatchers("/actuator/**").permitAll()
+          .requestMatchers("/actuator/**")
+          .permitAll()
           .anyRequest()
           .authenticated()
       }
